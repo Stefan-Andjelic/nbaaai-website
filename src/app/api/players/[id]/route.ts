b025d6f0f83e1e97ef7111317b14c16f74c2f19f
Stefan-Andjelic <stefanandjelic8@gmail.com>
2025-04-01 19:02:09 -0500

    added Promise to players/[id]/route.ts

--- src/app/api/players/[id]/route.ts
+++ src/app/api/players/[id]/route.ts
@@ -4,10 +4,10 @@ import { getPlayerDetails } from '@/lib/playerData';
 
 export async function GET(
   request: NextRequest, 
-  { params }: { params: { id: string } }
+  { params }: { params: Promise<{ id: string }> }
 ) {
   try {
-    const { id } = params;
+    const id = (await params).id;
 
     // Fetch player details
     const playerData = await getPlayerDetails(id);
